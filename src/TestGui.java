f19b78fe5fb028739b55b4168737902a2920b23a
Andrey.Metelyov <andrey.metelyov@gmail.com>
2020-09-24 15:33:04 +0900

    window listeners

--- src/TestGui.java
+++ src/TestGui.java
@@ -1,7 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class TestGui {
+    private JTextField textField;
+
+    class WindowListenerExample implements WindowListener{
+        @Override
+        public void windowOpened(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowClosed(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowIconified(WindowEvent e) {
+            textField.setText("windowIconified");
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent e) {
+            textField.setText("windowDeiconified");
+        }
+
+        @Override
+        public void windowActivated(WindowEvent e) {
+            textField.setText("windowActivated");
+        }
+
+        @Override
+        public void windowDeactivated(WindowEvent e) {
+            textField.setText("windowDeactivated");
+        }
+    }
+
+    class WindowAdapterExample extends WindowAdapter {
+        @Override
+        public void windowLostFocus(WindowEvent e) {
+            super.windowLostFocus(e);
+            textField.setText("windowLostFocus");
+        }
+
+        @Override
+        public void windowGainedFocus(WindowEvent e) {
+            super.windowGainedFocus(e);
+            textField.setText("windowGainedFocus");
+        }
+
+        @Override
+        public void windowActivated(WindowEvent e) {
+            super.windowActivated(e);
+            textField.setText("windowActivated");
+        }
+
+        @Override
+        public void windowDeactivated(WindowEvent e) {
+            super.windowDeactivated(e);
+            textField.setText("windowDeactivated");
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            super.windowClosing(e);
+
+        }
+    }
+
     public static void main(String[] args) {
         TestGui gui = new TestGui();
         gui.go();
@@ -10,13 +84,15 @@ public class TestGui {
     private void go() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        frame.addWindowListener(new WindowListenerExample());
+        frame.addWindowListener(new WindowAdapterExample());
         JPanel panel = new JPanel();
 
         SpringLayout springLayout = new SpringLayout();
         panel.setBackground(Color.darkGray);
         panel.setLayout(springLayout);
 
-        JTextField textField = new JTextField("Some text");
+        textField = new JTextField("Some text");
         JButton button = new JButton("Button1");
         JButton button2 = new JButton("Button2 shmutton");
 
